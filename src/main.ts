e9ea4f2a34c1de6ff1b0feb72b4f80a793b02641
Joker <40009719+pic123123@users.noreply.github.com>
2022-06-16 22:37:27 +0900

    123213

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const allowlist = [`http://localhost:3000`]; //프론트 주소
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: allowlist, // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.이거없으면 cors걸림
+  });
+
   await app.listen(6878);
 }
 bootstrap();
